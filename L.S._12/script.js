35acc660e55d91630e344e97eee0325cf1698da5
SergeyLalymenko <Lalymenko.Sergey153@gmail.com>
2020-06-12 10:47:42 +0300

    L.S._12

--- L.S._12/script.js
+++ L.S._12/script.js
@@ -6,18 +6,19 @@ function Accordion(list){
 }
 Accordion.prototype.onListClick = function(e){
     if(e.target.classList.contains('title')){
-        for(let i = 0; i < this.list.children.length; i++){
-            if(this.list.children[i] == e.target.parentElement && !e.target.parentElement.lastElementChild.classList.contains('body')){
-                e.target.parentElement.lastElementChild.classList.add('body');
-            } else if(this.list.children[i] == e.target.parentElement){
-                this.list.children[i].lastElementChild.classList.remove('body');
-            } else{
-                this.list.children[i].lastElementChild.classList.add('body');
-            }
+        this.changeClasses(e);
+    }
+}
+Accordion.prototype.changeClasses = function(e){
+    for(let i = 0; i < this.list.children.length; i++){
+        if(this.list.children[i] == e.target.parentElement && !e.target.parentElement.lastElementChild.classList.contains('body')){
+            e.target.parentElement.lastElementChild.classList.add('body');
+        } else if(this.list.children[i] == e.target.parentElement){
+            this.list.children[i].lastElementChild.classList.remove('body');
+        } else{
+            this.list.children[i].lastElementChild.classList.add('body');
         }
     }
 }
-
-
 
 const accordion = new Accordion(document.getElementById('list'));
