ad65cca740c1ea5dcf9b46f6eb74bdedf4fdb5ee
SergeyLalymenko <Lalymenko.Sergey153@gmail.com>
2020-06-24 22:23:08 +0300

    L.S._16

--- L.S._09(доп. задание)/script.js
+++ L.S._09(доп. задание)/script.js
@@ -1,49 +1,117 @@
-//function onBtnClick(){
-//    let text = textInput.value.trim();
-//    if(text !== ''){
-//        listUl.innerHTML += formTemplate
-//        .replace('{{textLi}}', text)
-//        .replace('{{textBtn}}', 'delete');
-//        textInput.value = '';
-//    }
-//}
+let URL = 'https://jsonplaceholder.typicode.com/todos';
+let TAG_NAME_LI = 'LI';
+let TAG_NAME_BUTTON = 'BUTTON';
+let CLASS_YELLOW_COLOR = 'yellow-color';
+let CLASS_GREEN_COLOR = 'green-color';
+let CLASS_YELLOW_GREEN_COLOR = 'yellow-color green-color';
+let BUTTON_TEXT_DELETE = 'delete';
+
 function onBtnClick(){
-    validValue();
+    checkValidValue();
 }
-function validValue(){
-    textInput.value = textInput.value.trim();
-    if(textInput.value !== ''){
+function checkValidValue(){
+    if(input.value !== ''){
         addLi();
     }
 }
 function addLi(){
+    input.value = input.value.trim();
+    id++;
     listUl.innerHTML += formTemplate
-    .replace('{{textLi}}', textInput.value)
-    .replace('{{textBtn}}', 'delete');
-    textInput.value = '';
+    .replace('{{textLi}}', input.value)
+    .replace('{{textBtn}}', BUTTON_TEXT_DELETE)
+    .replace('{{class}}', CLASS_YELLOW_COLOR)
+    .replace('{{id}}', id);
+    let todo = {
+        title: input.value,
+        completed: false,
+    }
+    clearInput();
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todo),
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+function clearInput(){
+    input.value = '';
 }
 function onLiElClick(e){
     switch(e.target.tagName){
-        case 'LI' : 
-            toggleGreenColor();
+        case TAG_NAME_LI : 
+            toggleGreenColor(e);
             break;
-        case 'BUTTON' :    
-            removeFatherEl();
+        case TAG_NAME_BUTTON :  
+            removeServerEl(e);  
+            removeFatherEl(e);
             break;
     }
-    function toggleGreenColor(){
-        e.target.classList.toggle('green-color');
-    }
-    function removeFatherEl(){
-        e.target.parentNode.remove();
-    }
+}
+function toggleGreenColor(e){
+    e.target.classList.toggle(CLASS_GREEN_COLOR);
+}
+function removeFatherEl(e){
+    e.target.parentNode.remove();
+}
+function removeServerEl(e){
+    fetch(URL + '/' + e.target.parentElement.id,{
+        method: 'DELETE',
+    })
 }
 
 
 
-let textInput = document.getElementById('main-input');
+
+function addServerTodos(data){
+    data.forEach((element => {
+        let readyTemplate;
+        if(element.completed == true){
+            generateGreenTodoHTML(readyTemplate, element);
+        } else{
+            generateYellowTodoHTML(readyTemplate, element);
+        }
+    }))
+}
+function generateGreenTodoHTML(readyTemplate, el){
+    readyTemplate = formTemplate
+    .replace('{{class}}', CLASS_YELLOW_GREEN_COLOR)
+    .replace('{{textLi}}', el.title)
+    .replace('{{textBtn}}', BUTTON_TEXT_DELETE)
+    .replace('{{id}}', el.id);
+    addReadyTemplate(readyTemplate);
+}
+function generateYellowTodoHTML(readyTemplate, el){
+    readyTemplate = formTemplate
+    .replace('{{class}}', CLASS_YELLOW_COLOR)
+    .replace('{{textLi}}', el.title)
+    .replace('{{textBtn}}', BUTTON_TEXT_DELETE)
+    .replace('{{id}}', el.id);
+    addReadyTemplate(readyTemplate);
+}
+function addReadyTemplate(template){
+    listUl.innerHTML += template;
+}
+
+
+
+
+
+
+let input = document.getElementById('main-input');
 let addBtn = document.getElementById('main-btn');
 let listUl = document.getElementById('main-ul');
 let formTemplate = document.getElementById('form-template').innerHTML;
+let id = 200;
+
 addBtn.addEventListener('click', onBtnClick);
 listUl.addEventListener('click', onLiElClick);
+
+fetch(URL)
+.then((res) => res.json())
+.then((data) => {
+    addServerTodos(data);
+});
